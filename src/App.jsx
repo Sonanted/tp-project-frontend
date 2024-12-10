7fba6f4c8a88cbdf00cc4622eca1d860333de89a
Andrey Nikonenko <sonanted@gmail.com>
2024-12-10 22:35:14 +0300

    build: Vite for React & some simple example components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import HelloWorldComponent from "./components/HelloWorld";
+import ImageComponent from "./components/Image";
+
+function App() {
+  return (
+    <>
+      <HelloWorldComponent></HelloWorldComponent>
+      <ImageComponent></ImageComponent>
+    </>
+  );
+}
+
+export default App;
+
---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,7 @@
+export default function HelloWorldComponent() {
+  return (
+    <>
+      <p>Hello, world!</p>
+    </>
+  );
+}
